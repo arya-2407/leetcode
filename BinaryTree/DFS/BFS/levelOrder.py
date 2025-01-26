365c6abc40e724ca2f2194eeb825188410174ffb
arya-2407 <aryapofficial@gmail.com>
2025-01-26 15:32:44 -0800

    level order

---
+++ BinaryTree/DFS/BFS/levelOrder.py
@@ -0,0 +1,24 @@
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        q = deque()
+        if root:
+            q.append(root)
+        
+        res = []
+
+        while len(q) > 0:
+            level = []
+            for i in range(len(q)):
+                cur = q.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            res.append(level)
+        return res
